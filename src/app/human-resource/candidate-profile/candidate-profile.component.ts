39562f9e6732502be30319f19b876480ae0d985a
Tamizharivu <tamizharivu.kumar@kovai.co>
2022-04-06 17:32:59 +0530

    Candidate view profile in Hr module completed

---
+++ src/app/human-resource/candidate-profile/candidate-profile.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Candidate } from 'src/app/model/candidate';
+import { Qualification } from 'src/app/model/qualification';
+import { Skill } from 'src/app/model/skill';
+import { CandidateService } from 'src/app/shared/candidate.service';
+
+@Component({
+  selector: 'app-candidate-profile',
+  templateUrl: './candidate-profile.component.html',
+  styleUrls: ['./candidate-profile.component.css']
+})
+export class CandidateProfileComponent implements OnInit {
+
+  public hrid!: string;
+  public candidate! : Candidate;
+  public candidateId! : string;
+  items: MenuItem[] = [];
+  photoLink!: string;
+  candidatePhoto: any;
+  candidateQualification: Qualification[] = [];
+  candidateSkills: Skill[] = [];
+
+
+  constructor(private route: Router,private candidateService: CandidateService) { }
+
+  ngOnInit(): void {
+
+    this.hrid = localStorage.getItem('id') as string;
+
+    this.candidateId  = this.route.url.split('/').pop()!;
+
+    this.getQualificationByCandidateId();
+
+    this.getSkillsByCandidateId();
+    
+    this.items = [
+      {label: 'My Job',icon: 'pi pi-briefcase', routerLink: "/humanresource/dashboard"},
+      { label: 'Profile', icon: 'pi pi-user', routerLink: "/humanresource/profile" },
+      { label: 'Logout', icon: 'pi pi-sign-out', routerLink: "/humanresource/logout" }
+    ];
+
+    this.candidateService.getCandidateById(this.candidateId).subscribe({
+      next: (data) =>{
+        this.candidate = data['data' as keyof object] as unknown as Candidate;
+        console.log(this.candidate);
+        this.candidatePhoto = this.candidate.candidatePhotoUrl;
+        if (this.candidate.candidatePhotoUrl == null) {
+          this.photoLink = "../../../assets/blank-profile.webp"
+        } else {
+          this.photoLink = this.candidate.candidatePhotoUrl
+        }
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    })
+  }
+
+  getQualificationByCandidateId() {
+    this.candidateService.getQualificationByCandidateId(this.candidateId).subscribe({
+      next: (data) => {
+        this.candidateQualification = data['data' as keyof Object] as unknown as Qualification[];
+        console.log(this.candidateQualification)
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+  getSkillsByCandidateId() {
+    this.candidateService.getSkillsByCandidateId(this.candidateId).subscribe({
+      next: (data) => {
+        this.candidateSkills = data['data' as keyof Object] as unknown as Skill[];
+        console.log(this.candidateSkills[0].skillSet.skillSetName);
+
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+
+  downloadMyFile() {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', this.candidate.candidateResume);
+    link.setAttribute('download', `resume.png`);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
+
+}
